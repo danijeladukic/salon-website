66aabbd27a5d6a6028313e93a26ff0845c04dffa
danijeladukic <danijeladolic7@gmail.com>
2023-03-27 23:28:34 +0200

    5th added slider

---
+++ src/components/ImageSlider.js
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+
+
+
+const ImageSlider= ({slides})=>{
+    const [currentIndex, setCurrentIndex] = useState(0)
+
+    const slideStyles={
+        width:'100%',
+        height:'100%',
+        borderRadius:'10px',
+        backgroundPosition:'center',
+        bckgroundSize:'cover',
+        backgroundImage: `url(${slides[currentIndex].url})`
+    
+    }
+    const sliderStyles ={
+        height:'100%',
+        position:'relative'//because of arrows
+    }
+    const leftArrowStyle={
+        position:'absolute',
+        top: '50%',
+        left: '5px',
+        transform: 'traslate(0, -50%)',
+        zIndex:'1',
+        fontSize:'45px',
+        color:'white',
+        cursor:'pointer'
+    }
+    const rightArrowStyle={
+        position:'absolute',
+        top: '50%',
+        right: '5px',
+        transform: 'traslate(0, -50%)',
+        zIndex:'1',
+        fontSize:'45px',
+        color:'white',
+        cursor:'pointer'
+    }
+    const goToPrevious = ()=>{
+        const isFirstSlide = currentIndex === 0
+        const newIndex = isFirstSlide ? slides.length - 1 : currentIndex-1
+        setCurrentIndex(newIndex)
+    }
+    const goToNext = () =>{
+        const isLastSlide = currentIndex === slides.length-1
+        const newIndex = isLastSlide ? 0 : currentIndex+1
+        setCurrentIndex(newIndex)
+    }
+
+    const dotsContainerStyles={
+        display:'flex',
+        flexDirection:'row',
+        justifyContent:'center'
+    }
+    const dotStyles={
+        margin:'1px 5px',
+        cursor: 'pointer',
+        fontSize:'25px',
+        color: 'white'
+
+    }
+
+    const goToSlide = (slideIndex)=>{
+        setCurrentIndex(slideIndex)
+    }
+    return(
+        <div style={sliderStyles}>
+            <div style={leftArrowStyle} onClick={goToPrevious}>◁</div>
+           
+            <div style={rightArrowStyle} onClick={goToNext}>▷</div>
+
+            <div style={slideStyles}></div>
+
+            <div style ={dotsContainerStyles}>
+                {slides.map((slide, slideIndex)=>(
+                    <div key ={slideIndex} style={dotStyles} onClick={ ()=> goToSlide(slideIndex)}>○</div>
+                    
+                ))}
+            </div>
+
+
+                
+            
+        </div>
+
+
+    )
+}
+
+export default ImageSlider;
--- src/components/MyWorkBar.js
+++ src/components/MyWorkBar.js
@@ -1,12 +1,33 @@
 import React from "react";
 import '../styles/MyWorkBar.css'
+import ImageSlider from '../components/ImageSlider.js'
+
 
 function MyWorkBar(){
+    const slides = [
+        {url:'https://i.pinimg.com/564x/56/1b/82/561b8216b9d04f92ba01e8edab91b1d0.jpg', title:'a'},
+        {url:'https://i.pinimg.com/564x/be/95/90/be95903d0082721ff3ddabab882e8c48.jpg', title:'a'},
+        {url:'https://i.pinimg.com/564x/ad/28/61/ad2861ceb6ef7392965731a49f2fddf3.jpg', title:'a'},
+        {url:'https://i.pinimg.com/564x/59/cf/34/59cf345b4fef0a72d53b95e228dbe0ae.jpg', title:'a'},
+        {url:'https://i.pinimg.com/564x/db/f0/ea/dbf0eab2b33f5450f9dda9b871f239d5.jpg', title:'a'},
+    
+    ]
+    const containerStyle ={
+            width: '700px',
+            height: '480px',
+            margin:'0 auto'
+    }
+    
     return(
-
         <div className="mywork-bar">
-        
-           <img src="../../images/mywork.png"></img>
+            <div style={containerStyle}>
+                <ImageSlider slides={slides}>
+                
+                </ImageSlider>
+            </div>
+
+
+           {/* <img src="../../images/mywork.png"></img>
         
        <div className="main-container">
            <div className="photo-container">
@@ -21,7 +42,10 @@ function MyWorkBar(){
            <img className="photo right-up" src="../../images/photo7.jpg" alt="photo"></img> 
            <img className="photo left-up right-up" src="../../images/photo8.jpg" alt="photo"></img> 
          </div>
-        </div>
+        </div> */}
+
+
+
         </div>
 
 
